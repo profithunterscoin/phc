39ed80c5ff85350d68d1fe0203809c3dd4148b99
Justin Percy <justin@biznaturally.ca>
2019-01-06 18:32:33 -0600

    1.0.0.7

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -11,7 +11,9 @@
 #include "sync.h"
 #include "wallet.h"
 
+#define BOOST_NO_CXX11_SCOPED_ENUMS
 #include <boost/filesystem.hpp>
+#undef BOOST_NO_CXX11_SCOPED_ENUMS
 #include <boost/foreach.hpp>
 
 using namespace std;
