4c810dd5e46f1db6af38b77bcbd5ce5f4746a51c
Justin Percy <justin@biznaturally.ca>
2019-04-01 23:22:09 -0500

    1.0.0.7

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -200,6 +200,8 @@ extern json_spirit::Value firewallfloodingwalletminbytes(const json_spirit::Arra
 extern json_spirit::Value firewallfloodingwalletmaxbytes(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value firewallfloodingwalletattackpatternadd(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value firewallfloodingwalletattackpatternremove(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value firewallfloodingwalletattackignoredadd(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value firewallfloodingwalletattackignoredremove(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value firewallfloodingwalletmintrafficavg(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value firewallfloodingwalletmaxtrafficavg(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value firewallfloodingwalletmincheck(const json_spirit::Array& params, bool fHelp);
