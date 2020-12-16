52b1fed18abe7787c6c82db761cd6358b2bbbd50
Justin Percy <justinpercy@gmail.com>
2020-12-15 22:09:03 -0600

    1.0.0.7

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -38,7 +38,7 @@ void CActiveMasternode::ManageStatus()
     //need correct adjusted time to send ping
     bool fIsInitialDownload = IsInitialBlockDownload();
     
-    if(fIsInitialDownload && GetTime() - pindexBest->nTime > 600)
+    if(fIsInitialDownload)
     {
         status = MASTERNODE_SYNC_IN_PROCESS;
 
